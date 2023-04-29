d32f4e140c242465f662ade93c5e7ac6628ff351
Stephan Sturges <stephansturges@Stephans-Air.lan>
2023-04-29 10:28:44 +0200

    ga

--- services/openai.py
+++ services/openai.py
@@ -4,45 +4,45 @@ import replicate
 
 from tenacity import retry, wait_random_exponential, stop_after_attempt
 
+# @retry(wait=wait_random_exponential(min=1, max=20), stop=stop_after_attempt(15))
+# def get_embeddings(texts: List[str]) -> List[List[float]]:
+#     embeddings_list = []
+    
+#     for text in texts:
+#         output = replicate.run(
+#             "replicate/all-mpnet-base-v2:a441e62645d851373aa8a000e1471e8ac2f7c61a4b4148984bc190fc0b4c1f03",
+#             input={"text": text }
+#         )
+        
+#         embeddings = output[0]["embedding"]
+#         embeddings_list.append(embeddings)
+#         print("got results from replicate")
+#         print(str(embeddings))
+    
+#     return embeddings_list
+
 @retry(wait=wait_random_exponential(min=1, max=20), stop=stop_after_attempt(3))
 def get_embeddings(texts: List[str]) -> List[List[float]]:
-    embeddings_list = []
-    
-    for text in texts:
-        output = replicate.run(
-            "replicate/all-mpnet-base-v2:a441e62645d851373aa8a000e1471e8ac2f7c61a4b4148984bc190fc0b4c1f03",
-            input={"text": text }
-        )
-        
-        embeddings = output[0]["embedding"]
-        embeddings_list.append(embeddings)
-        print("got results from replicate")
-        print(str(embeddings))
-    
-    return embeddings_list
+    """
+    Embed texts using OpenAI's ada model.
 
-# @retry(wait=wait_random_exponential(min=1, max=20), stop=stop_after_attempt(3))
-# def get_embeddings(texts: List[str]) -> List[List[float]]:
-#     """
-#     Embed texts using OpenAI's ada model.
+    Args:
+        texts: The list of texts to embed.
 
-#     Args:
-#         texts: The list of texts to embed.
+    Returns:
+        A list of embeddings, each of which is a list of floats.
 
-#     Returns:
-#         A list of embeddings, each of which is a list of floats.
+    Raises:
+        Exception: If the OpenAI API call fails.
+    """
+    # Call the OpenAI API to get the embeddings
+    response = openai.Embedding.create(input=texts, model="text-embedding-ada-002")
 
-#     Raises:
-#         Exception: If the OpenAI API call fails.
-#     """
-#     # Call the OpenAI API to get the embeddings
-#     response = openai.Embedding.create(input=texts, model="text-embedding-ada-002")
+    # Extract the embedding data from the response
+    data = response["data"]  # type: ignore
 
-#     # Extract the embedding data from the response
-#     data = response["data"]  # type: ignore
-
-#     # Return the embeddings as a list of lists of floats
-#     return [result["embedding"] for result in data]
+    # Return the embeddings as a list of lists of floats
+    return [result["embedding"] for result in data]
 
 
 @retry(wait=wait_random_exponential(min=1, max=20), stop=stop_after_attempt(3))
