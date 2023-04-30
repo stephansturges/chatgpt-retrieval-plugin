a48407e8bdf2a3adaf1a66d1e2d45ce206f145ee
Stephan Sturges <stephansturges@Stephans-Air.lan>
2023-04-30 13:46:48 +0200

    adding return

--- services/openai.py
+++ services/openai.py
@@ -42,6 +42,7 @@ def get_embeddings(texts: List[str]) -> List[List[float]]:
     data = response["data"]  # type: ignore
 
     # Return the embeddings as a list of lists of floats
+    print([result["embedding"] for result in data])
     return [result["embedding"] for result in data]
 
 
